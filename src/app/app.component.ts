35efbf4dbc0a621c7f3a341543bb27764f0328b9
Ivar Mahhonin <ivar.mahhonin@gmail.com>
2019-02-20 16:39:30 +0200

    simple version of angular infinite scroll

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import {Component} from '@angular/core';
-import {times, chunk} from 'lodash';
-import {BehaviorSubject, Observable, ReplaySubject, Subject, Subscription} from 'rxjs';
-import {CollectionViewer, DataSource} from '@angular/cdk/collections';
+import {times} from 'lodash';
+import {BehaviorSubject} from 'rxjs';
 
 interface IItem {
     title: string;
@@ -17,64 +16,20 @@ const ITEMS_COUNT = 10000;
     styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-    dataSource = new ItemDataSource();
+    itemsStream$: BehaviorSubject<Array<IItem>> = new BehaviorSubject<Array<IItem>>(this.generateItems());
     scrolledIndex = 0;
 
-    scrolledIndexChange(i) {
-        this.scrolledIndex = i;
-    }
-
-    titleTrackFn = (_: number, item: IItem) => item.title;
-}
-
-
-class ItemDataSource extends DataSource<IItem> {
-    private PAGE_SIZE = 20;
-    private fetchedPages = new Set<number>();
-    private cachedData = this.generateItems();
-    private dataStream = new BehaviorSubject<Array<IItem>>(times(1000));
-    private subscription = new Subscription();
-
-    connect(collectionViewer: CollectionViewer): Observable<Array<IItem>> {
-        this.subscription.add(collectionViewer.viewChange.subscribe(range => {
-            console.log(range);
-            const startPage = this.getPageForIndex(range.start);
-            const endPage = this.getPageForIndex(range.end);
-            for (let i = startPage; i <= endPage; i++) {
-                this.fetchPage(i);
-            }
-        }));
-
-        return this.dataStream;
-    }
-
-    disconnect(): void {
-        this.subscription.unsubscribe();
-    }
-
-    private getPageForIndex(index: number): number {
-        return Math.floor(index / this.PAGE_SIZE);
-    }
-
-    private fetchPage(page: number) {
-        if (this.fetchedPages.has(page)) {
-            return;
-        }
-        this.fetchedPages.add(page);
-
-        const chunked = chunk(this.cachedData, this.PAGE_SIZE);
-        console.log('chunked size', chunked.length);
-        const pageItems = chunked[page];
-        console.log('pageItems size', pageItems);
-
-        this.dataStream.next(pageItems);
-    }
-
-    private generateItems(): Array<IItem> {
+    generateItems(): Array<IItem> {
         return times(ITEMS_COUNT, Number).map(i => ({
             title: `Image #${i}`,
             src: `http://lorempixel.com/200/200/`,
             blur: `https://picsum.photos/200/200?blur`
         }))
     }
+
+    scrolledIndexChange(i): void {
+        this.scrolledIndex = i;
+    }
+
+    titleTrackFn = (_: number, item: IItem) => item.title;
 }
